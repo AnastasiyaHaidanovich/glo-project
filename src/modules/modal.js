ef6f49538d144fb252da4995a97dfff13f9c3261
AnastasiyaHaidanovich <Nastyal0ve9517@gmail.com>
2021-12-22 16:03:44 +0300

    add: animate modals

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -5,14 +5,63 @@ const modal = () => {
 
     buttons.forEach(btn => {
         btn.addEventListener("click", () => {
-            modal.style.display = "block";
+            const smoothOpen = () => {
+                modal.style.display = "block";
+                modal.style.opacity = 0;
+
+                let idInterval;
+                let count = 0;
+
+                const appear = () => {
+                    if (count <= 1){
+                    count += 0.05;
+                    modal.style.opacity = `${count}`;
+                    idInterval = requestAnimationFrame(appear);
+                } else {
+                    cancelAnimationFrame(idInterval);
+                }
+                };
+                idInterval = requestAnimationFrame(appear);
+            };
+            
+            if(window.innerWidth > 768){
+                smoothOpen();
+            } else {
+                modal.style.display = "block";
+            }
+
+            // setInterval(appear, 50);
+        });
+
+        modalClose.addEventListener("click", () => {
+            const smoothClose = () => {
+                let idInterval;
+                let count = 1;
+
+                const close = () => {
+                    if (count >= 0){
+                    count -= 0.05;
+                    modal.style.opacity = `${count}`;
+                    idInterval = requestAnimationFrame(close);
+                } else {
+                    cancelAnimationFrame(idInterval);
+                    modal.style.display = "none";
+                }
+                };
+                idInterval = requestAnimationFrame(close);
+            };
+
+            if (window.innerWidth > 768){
+                smoothClose();
+            } else {
+                modal.style.display = "none";
+            }
+            
         });
     });
 
-    modalClose.addEventListener("click", () => {
-        modal.style.display = "none";
-    });
-    // console.log(modal);
+    
+    console.log(window.innerWidth);
 };
 
 export default modal;
