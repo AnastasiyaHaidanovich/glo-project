1f10a6ec610d404f54c5b62a9b5c39143c048348
AnastasiyaHaidanovich <Nastyal0ve9517@gmail.com>
2022-01-04 13:00:11 +0300

    add: use XMLHttpRequest

--- src/modules/ajaxRequests.js
+++ src/modules/ajaxRequests.js
@@ -1,33 +1,50 @@
 const request = () => {
-    const getData = (url, urlSend) => {
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                sendData(urlSend, JSON.stringify(data));
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    };
+    // const getData = (url, urlSend) => {
+    //     fetch(url)
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log(data);
+    //             sendData(urlSend, JSON.stringify(data));
+    //         })
+    //         .catch(error => {
+    //             console.log(error);
+    //         });
+    // };
 
-    const sendData = (url, dataSend) => {
-        return fetch(url, {
-            method: "POST",
-            body: dataSend,
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-            },
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    };
-    getData("db.json", "https://jsonplaceholder.typicode.com/posts");
+    // const sendData = (url, dataSend) => {
+    //     return fetch(url, {
+    //         method: "POST",
+    //         body: dataSend,
+    //         headers: {
+    //             "Content-type": "application/json; charset=UTF-8",
+    //         },
+    //     })
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log(data);
+    //         })
+    //         .catch(error => {
+    //             console.log(error);
+    //         });
+    // };
+    // getData("db.json", "https://jsonplaceholder.typicode.com/posts");
+
+    let json = JSON.stringify({
+        name: "Вася",
+        surname: "Петров"
+      });
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://jsonplaceholder.typicode.com/posts");
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhr.send(json);
+
+    xhr.onload = function() {
+       console.log(`Загружено: ${xhr.response}`);
+      };
+      
+      xhr.onerror = function() {
+        console.log(`Ошибка соединения`);
+      };
 };
 
 export default request;
