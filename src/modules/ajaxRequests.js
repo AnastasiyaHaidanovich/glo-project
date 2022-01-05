7b1093b8a39474193ed69e5a6db389b5bf3e59bd
AnastasiyaHaidanovich <Nastyal0ve9517@gmail.com>
2022-01-05 09:44:27 +0300

    fix: httpRequest

--- src/modules/ajaxRequests.js
+++ src/modules/ajaxRequests.js
@@ -6,56 +6,52 @@ const request = () => {
             .then(data => {
                 console.log(data);
                 newData = data;
-                console.log(newData);
             })
             .catch(error => {
                 console.log(error);
             });
     };
 
-    const sendData = (url, data) => {
-        return fetch(url, {
-            method: "POST",
-            body: JSON.stringify(data),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-            },
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    };
+    // const sendData = (url, data) => {
+    //     return fetch(url, {
+    //         method: "POST",
+    //         body: JSON.stringify(data),
+    //         headers: {
+    //             "Content-type": "application/json; charset=UTF-8",
+    //         },
+    //     })
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log(data);
+    //         })
+    //         .catch(error => {
+    //             console.log(error);
+    //         });
+    // };
 
     getData("db.json");
     
-    setTimeout(() => {
-         sendData("https://jsonplaceholder.typicode.com/posts", newData);
-    }, 1000);
+    // setTimeout(() => {
+    //      sendData("https://jsonplaceholder.typicode.com/posts", newData);
+    // }, 1000);
    
     
     
-    
+    setTimeout(() => {
+        let xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://jsonplaceholder.typicode.com/posts");
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
 
-    // let json = JSON.stringify({
-    //     name: "Вася",
-    //     surname: "Петров"
-    //   });
-    // let xhr = new XMLHttpRequest();
-    // xhr.open("POST", "https://jsonplaceholder.typicode.com/posts");
-    // xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    // xhr.send(json);
+    xhr.send(JSON.stringify(newData));
 
-    // xhr.onload = function() {
-    //    console.log(`Загружено: ${xhr.response}`);
-    //   };
+    xhr.onload = function() {
+       console.log(`Загружено: ${xhr.response}`);
+      };
       
-    //   xhr.onerror = function() {
-    //     console.log(`Ошибка соединения`);
-    //   };
+      xhr.onerror = function() {
+        console.log(`Ошибка соединения`);
+      };
+    }, 1000);
 };
 
 export default request;
